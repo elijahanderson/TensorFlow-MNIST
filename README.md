1ebaac3a60010a4b820affafd821d3a78a91b666
Elijah Anderson <elijahanderson_2019@depauw.edu>
2018-02-28 16:29:58 -0500

    NN Implemented
    - updated README to compare results of from-scratch NN to tensorflow NN
    
    The tensorflow NN has now been properly implemented, but I still need to:
    1) test the tensorflow NN with the sigmoid cost function
    2) create visualization graphs showing how the algorithm classified digits using a pixel heatmap

--- README.md
+++ README.md
@@ -11,3 +11,151 @@ will be using a tutorial by Justin Francis from a little over a year ago:
 https://www.oreilly.com/learning/not-another-mnist-tutorial-with-tensorflow
 
 I chose this one because it is visually appealing and interactive, so I really learn rather than just copying code.
+
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=---=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=
+
+Sample result of training 50,000 images with the tensorflow NN with a learning rate of 1.0 and a batch size of 30:
+
+Batch no.: 0
+Accuracy = 0.6668
+
+Batch no.: 1
+Accuracy = 0.9118
+
+Batch no.: 2
+Accuracy = 0.9174
+
+Batch no.: 3
+Accuracy = 0.9186
+
+Batch no.: 4
+Accuracy = 0.9207
+
+Batch no.: 5
+Accuracy = 0.9212
+
+Batch no.: 6
+Accuracy = 0.9212
+
+Batch no.: 7
+Accuracy = 0.9217
+
+Batch no.: 8
+Accuracy = 0.9219
+
+Batch no.: 9
+Accuracy = 0.9222
+
+Batch no.: 10
+Accuracy = 0.9221
+
+Batch no.: 11
+Accuracy = 0.9223
+
+Batch no.: 12
+Accuracy = 0.923
+
+Batch no.: 13
+Accuracy = 0.9229
+
+Batch no.: 14
+Accuracy = 0.9232
+
+Batch no.: 15
+Accuracy = 0.9231
+
+Batch no.: 16
+Accuracy = 0.923
+
+Batch no.: 17
+Accuracy = 0.9231
+
+Batch no.: 18
+Accuracy = 0.9234
+
+Batch no.: 19
+Accuracy = 0.9234
+
+Batch no.: 20
+Accuracy = 0.9235
+
+Batch no.: 21
+Accuracy = 0.9235
+
+Batch no.: 22
+Accuracy = 0.9236
+
+Batch no.: 23
+Accuracy = 0.9237
+
+Batch no.: 24
+Accuracy = 0.9238
+
+Batch no.: 25
+Accuracy = 0.9238
+
+Batch no.: 26
+Accuracy = 0.9243
+
+Batch no.: 27
+Accuracy = 0.9243
+
+Batch no.: 28
+Accuracy = 0.9245
+
+Batch no.: 29
+Accuracy = 0.9249
+
+Batch no.: 30
+Accuracy = 0.9251
+
+So, it classified 9251 / 10000 on its last batch.
+
+For comparison, a sample result of training 50,000 with the from-scratch NN with the same parameters:
+
+Number correct before network is implemented: 1056 / 10000
+Epoch 0 : 7548 / 10000
+Epoch 1 : 9034 / 10000
+Epoch 2 : 9162 / 10000
+Epoch 3 : 9218 / 10000
+Epoch 4 : 9257 / 10000
+Epoch 5 : 9308 / 10000
+Epoch 6 : 9339 / 10000
+Epoch 7 : 9361 / 10000
+Epoch 8 : 9379 / 10000
+Epoch 9 : 9389 / 10000
+Epoch 10 : 9406 / 10000
+Epoch 11 : 9398 / 10000
+Epoch 12 : 9427 / 10000
+Epoch 13 : 9447 / 10000
+Epoch 14 : 9437 / 10000
+Epoch 15 : 9423 / 10000
+Epoch 16 : 9447 / 10000
+Epoch 17 : 9444 / 10000
+Epoch 18 : 9440 / 10000
+Epoch 19 : 9450 / 10000
+Epoch 20 : 9454 / 10000
+Epoch 21 : 9463 / 10000
+Epoch 22 : 9468 / 10000
+Epoch 23 : 9478 / 10000
+Epoch 24 : 9459 / 10000
+Epoch 25 : 9479 / 10000
+Epoch 26 : 9470 / 10000
+Epoch 27 : 9471 / 10000
+Epoch 28 : 9478 / 10000
+Epoch 29 : 9474 / 10000
+
+So, it classified 9474 / 10000 on its last batch (classifying 9478 at its peak).
+
+From this, it's easy to conclude that perhaps the from-scratch NN is better at classification than tensorflow's
+implementation of a NN. However, remember that that the from-scratch uses the sigmoid function as its cost function,
+whereas the tensorflow implementation uses the softmax function. Here's a comparison between the two:
+
+    Sigmoid: S(x) = 1 / (1 + e^(-x))
+    Softmax: S(x) = e^x / (e^max(x) * sum(e^x / e^max(x)))
+
+So softmax is clearly more complicated, but does that mean it's better? To get a better comparison, let's run the
+tensorflow neural network again, except using the sigmoid function as the cost function:
+
+[TO BE CONTINUED]
+
--- tf_mnist.py
+++ tf_mnist.py
@@ -1,22 +1,9 @@
+
 """
-    Last semester, I implemented a from-scratch neural network to classify handwritten digits from the MNIST dataset.
-    This semester, my goal is to become fluent with the machine learning framework TensorFlow. I hope to have at least 3
-    different machine learning projects up on my github using the framework by the time I start my internship at Cerner.
-
-    The first project will be a simple one to get a feel for the framework and start to really understand how to
-    manipulate and interact with the code rather than just stealing others' code. This project will be a tensorflow
-    implementation of the project I worked on last semester, classifying handwritten digits from the MNIST dataset. I
-    will be using a tutorial by Justin Francis from a little over a year ago:
-
-    https://www.oreilly.com/learning/not-another-mnist-tutorial-with-tensorflow
-
-    I chose this one because it is visually appealing and interactive, so I really learn rather than just copying code.
-    So without further ado....
-
     TENSORFLOW MNIST -- by Eli Anderson
 """
 
-# import  the MNIST dataset (tensorflow has it conveniently built-in)
+# import the MNIST dataset (tensorflow has it conveniently built-in)
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 # one_hot encoding: 5 = 0000010000, 2 = 0010000000, etc. This is how the from-scratch model worked as well
@@ -33,11 +20,11 @@ def train_size(num) :
     print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
     print('Total training images in dataset = ' + str(mnist.train.images.shape))
 
-    # x_train = the pixel data itself
+    # `x_train` = the pixel data itself
     x_train = mnist.train.images[:num,:]
     print('x_train examples loaded = ' + str(x_train.shape))
 
-    # y_train = the image labels
+    # `y_train` = the image labels
     y_train = mnist.train.labels[:num,:]
     print('y_train examples loaded = ' + str(y_train.shape))
     print('\n')
@@ -59,7 +46,7 @@ def display_digit(num) :
     print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
     print('Digit\'s one_hot encoding: ' + str(y_train[num]))
 
-    # since labels are stored in one_hot lists, use argmax to return the index, which is the actual label
+    # since labels are stored in one_hot lists, use argmax() to return the index, which is the actual label
     label = y_train[num].argmax(axis=0)
     # images are 28x28 pixels
     image = x_train[num].reshape([28,28])
@@ -67,8 +54,8 @@ def display_digit(num) :
     plt.imshow(image, cmap=plt.get_cmap('gray_r'))
     plt.show()
 
-# time to build and train the model
-x_train, y_train = train_size(10000)
+# time to build the model
+x_train, y_train = train_size(50000)
 # some arbitrary digit
 display_digit(2753)
 
@@ -79,10 +66,92 @@ display_digit(2753)
 sess = tf.Session()
 
 # define a placeholder -- a variable used to feed data into (i.e., the input layer). Its shape and type need to be
-# matched exactly. Let's define x as the placeholder to feed the x_train data into:
+# matched exactly. Let's define x as the placeholder to feed the `x_train` data into:
 x = tf.placeholder(tf.float32, shape=[None, 784])
 #  the input layer can be fed as many examples as you want(None) of 784-sized values
 
-# define y_ as the placeholder to feed the y_train data into. This will just be used later to compare our predictions to
+# define `y_` as the placeholder to feed the y_train data into. This will just be used later to compare our predictions to
 # ground truths
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+""" 
+ next is to define the weights and biases. Remember, the whole point of machine learning is to change these values to
+ make them as low as possible, which classifies different things. E.g. the hill analogy for stochastic gradient descent
+ The values will be initialized to zero, since tensorflow optimizes these values for us. Much less work than from-scratch.
+"""
+
+# `W` is a collection of 784 values for each of the 10 classes
+W = tf.Variable(tf.zeros([784, 10]))
+# and each of the 10 classes gets a bias
+b = tf.Variable(tf.zeros([10]))
+
+# now, define our classifier function. The tutorial uses multinomial logistic regression, but there are plenty more
+# methods, such as the sigmoid function I used in the from-scratch NN
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+# `tf.matmul(x, W) + b` returns the number of training examples fed * the number of classes. In this case, 784x10
+print(y)
+
+"""
+ to actually get the values of y, we need to run an appropriate session and feed it some actual data
+ in order to run a function in the session, the variables must be initialized within it:
+ # sess.run(tf.global_variables_initializer())
+ # print(sess.run(y, feed_dict={x: x_train}))
+ our classifier knows nothing at this point, so the probability for each example is 10%
+"""
+
+# next, define the cost function, which measures the accuracy of the classifier by comparing the true values from
+# `y_train` to the results of our predictions (y)
+cost_func = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+# `tf.reduce_mean` & `tf.reduce_sum` just compute the mean/sum across the dimensions of the given tensor
+
+""" 
+the function is taking the log of all our predictions (y) and multiplying by the example's true value (y_)
+if that result for each value is close to 0, it will make the value a large negative number, and vice versa:
+   e.g.: -log(.01) = 4.6 ; -log(.99) = .1
+therefore, we are penalizing the classifier with a large number if the prediction is confidently incorrect and a very
+small number if the prediction is confidently correct
+
+An example of a softmax prediction that is very confident that the digit is a 3:
+    [0.03, 0.03, 0.01, 0.9, 0.01, 0.01, 0.0025,0.0025, 0.0025, 0.0025]
+Digits: 0    1     2    3     4     5      6      7       8       9
+-log(.9) = .046
+-log(.0025) = 2.6
+See how that works? 
+
+---------------------------------------------------------------------------------------------------------------------
+
+Now we begin to actually train our classifier. This is done by continuously assigning appropriate values to W and b
+such that the lowest possible loss is achieved. 
+"""
+
+# We already have `x_train`/`y_train` set up, so we just need to assign x_test/y_test, our learning rate, and our batch size
+x_test, y_test = test_size(10000)
+learning_rate = 1.0
+train_steps = 3000
+
+# initialize all variable to be used by our tensorflow graph
+sess.run(tf.global_variables_initializer())
+
+# Now, we need to train our classifier using stochastic gradient descent, like I did in the from-scratch NN.
+# `training` will perform the SGD optimizer with a chosen learning rate, and try to minimize with our cost function
+training = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost_func)
+
+# `correction_prediction` will return a tensor of type bool, depending on our classifier function's (y) greatest value
+# (what it thinks is the correct one is equal to the actual value)
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+# `accuracy` is rather self-explanatory
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+"""
+    Time to train. We'll define a loop that repeats batch_size times.
+    For each loop, it runs `training`, feeding in values from `x_train` and `_train` using feed_dict, similar to the
+    example shown earlier. It will also calculate accuracy for each unseen example in `x_test` as it trains the examples
+    in `x_train`
+"""
+
+for i in range(train_steps+1) :
+    sess.run(training, feed_dict={x: x_train, y_: y_train})
+    # for every 100 examples trained, recalculate the accuracy
+    if i%100 == 0 :
+        print('\nBatch no.: ' + str(i//100) + '\nAccuracy = ' + str(sess.run(accuracy, feed_dict={x: x_test, y_: y_test})) +
+              '\nLoss = ' + str(sess.run(cost_func, {x: x_train, y_: y_train})))
